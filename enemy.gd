53953240940a191828cde0dd01d8bd492a1d0304
awesomesauce <duongdylan2008@gmail.com>
2025-10-29 11:16:54 -0500

    sigmas
    I can make literally everything else now so hecks yes (keeping it school appropriate)

--- enemy.gd
+++ enemy.gd
@@ -8,6 +8,6 @@ func check_collisions(body):
 	if body.is_in_group("Player Hitbox") && !invincible:
 		statemach.transition(launch_state)
 		invincible = true
-		print(sqrt(abs(PlayerGlobals.launchVector.x+PlayerGlobals.launchVector.y))/30)
-		i_frame_timer.start ( sqrt(abs(PlayerGlobals.launchVector.x+PlayerGlobals.launchVector.y))/30 ) 
+		print(sqrt(abs(PlayerGlobals.launchVector.x+PlayerGlobals.launchVector.y))/50)
+		i_frame_timer.start ( sqrt(abs(PlayerGlobals.launchVector.x+PlayerGlobals.launchVector.y))/50 ) 
 		#ouchie wouchie
